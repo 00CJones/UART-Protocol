a68a1d6248f1b964ea2f3c51d404d855bd582154
Calvin <chopin19@byui.edu>
2025-01-06 11:49:53 -0700

    update

---
+++ UARTLab.srcs/sources_1/new/UART_top.v
@@ -0,0 +1,45 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/30/2024 10:01:38 PM
+// Design Name: 
+// Module Name: UART_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module UART_top(
+    input clk,
+    input btnC,
+    input btnU,
+    output [6:0] seg,
+    output dp,
+    output [3:0] an,
+    input [15:0] sw,
+    output TX, // JA[4]
+    input RX,  // JA[3]
+    output [7:0] led
+    );
+    wire baud_clk;
+    wire [7:0]parallel_data_rcv;
+    assign led = parallel_data_rcv;
+    
+    clk_gen2 clk_115200(clk, btnU, baud_clk);
+    Transmit sendData(btnC, sw[7:0], TX, baud_clk, clk);
+    Receive receiveData(RX, parallel_data_rcv, clk);
+
+    sseg_x4_top displayReceivedData(parallel_data_rcv, seg, an, dp, btnC, clk);
+    
+endmodule
